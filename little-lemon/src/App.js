762046652b5e80f9d540e499c5b3edb97232db02
Memona <memonamahroo@mail.com>
2023-09-29 04:24:33 +0200

    ready to use the app from the scratch

---
+++ little-lemon/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      Little Lemon   
+      </h1>
+      <p>
+        Website in progress!
+      </p>
+    </div>
+  );
+}
+
+export default App;
